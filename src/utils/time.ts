2ead87ad2dd48b5284839f697aa63f65cc55d840
jamesdanielwhitford <james.whitford0@gmail.com>
2026-03-05 12:32:33 +0200

    clear branch for benchmarks-2026 rebuild

--- src/utils/time.ts
+++
@@ -1,4 +0,0 @@
-export const isoUtcNow = () => new Date().toISOString();
-export const nowNs = () => process.hrtime.bigint();
-export const msSince = (start: bigint) =>
-  Number((process.hrtime.bigint() - start) / 1_000_000n);
